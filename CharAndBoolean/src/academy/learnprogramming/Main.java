11b85c12767274f1dee3762f97e74a6e784abeb1
abhishekchandra2522k <1ac23456789@gmail.com>
2021-12-25 22:34:56 +0530

    Add CharAndBoolean

---
+++ CharAndBoolean/src/academy/learnprogramming/Main.java
@@ -0,0 +1,17 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        char myChar = 'D';
+        char myUnicode = '\u00A9';
+
+        System.out.println(myChar + " " + myUnicode);
+
+        boolean myTrueBoolValue = true;
+        boolean myFalseBoolValue = false;
+
+        boolean isCustomerOver18 = true;
+    }
+}
