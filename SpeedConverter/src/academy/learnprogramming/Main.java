394275b9de6048bae8612e31316ab1845960e74d
abhishekchandra2522k <1ac23456789@gmail.com>
2022-01-12 13:35:05 +0530

    Update SpeedConverter, add seperate SpeedConverter class

--- SpeedConverter/src/academy/learnprogramming/Main.java
+++ SpeedConverter/src/academy/learnprogramming/Main.java
@@ -4,31 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
-        printConversion(1.5);
-        printConversion(10.25);
-        printConversion(-5.6);
-        printConversion(25.42);
-        printConversion(75.114);
-    }
-
-    public static long toMilesPerHour(double kilometersPerHour)
-    {
-        if(kilometersPerHour < 0)
-        {
-            return -1;
-        }
-        return Math.round(kilometersPerHour * 0.6215d);
-    }
-
-    public static void printConversion(double kilometersPerHour)
-    {
-        if(kilometersPerHour < 0)
-        {
-            System.out.println("Invalid Value");
-        }
-        else
-        {
-            System.out.println(kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h");
-        }
+        SpeedConverter.printConversion(1.5);
+        SpeedConverter.printConversion(10.25);
+        SpeedConverter.printConversion(-5.6);
+        SpeedConverter.printConversion(25.42);
+        SpeedConverter.printConversion(75.114);
     }
 }
