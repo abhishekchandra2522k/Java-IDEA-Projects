6fb1a5ffb1549d73f59c2d0c0621fa8042659d0a
abhishekchandra2522k <1ac23456789@gmail.com>
2022-03-02 18:17:04 +0530

    Add ObjectOrientatedProgrammingClasses

---
+++ ObjectOrientatedProgrammingClasses/src/academy.learnprogramming/Main.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming;
+
+public class Main {
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car holden = new Car();
+        System.out.println("Porsche Model: " + porsche.getModel());
+        porsche.setModel("Carrera");
+        System.out.println("Porsche Model: " + porsche.getModel());
+
+        holden.setModel("Commodore");
+        System.out.println("Holden Model: " + holden.getModel());
+    }
+}
