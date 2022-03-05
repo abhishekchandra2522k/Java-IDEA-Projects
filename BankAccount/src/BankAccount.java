69250da4eddbda6a0af75ac28899779d0b74b35b
abhishekchandra2522k <1ac23456789@gmail.com>
2022-03-05 18:01:37 +0530

    Update BankAccount

--- BankAccount/src/BankAccount.java
+++ BankAccount/src/BankAccount.java
@@ -5,14 +5,22 @@ public class BankAccount {
     private String email;
     private String phoneNumber;
 
-    // Constructor
-//    public void BankAccount(String accountNumber, long balance, String customerName, String email, String phoneNumber){
-//        this.accountNumber = accountNumber;
-//        this.balance = balance;
-//        this.customerName = customerName;
-//        this.email = email;
-//        this.phoneNumber = phoneNumber;
-//    }
+    public BankAccount(){
+        this("2872", 0, "NULL", "NULL", "NULL");
+        System.out.println("Empty constructor called");
+    }
+
+    public BankAccount(String accountNumber, long balance, String customerName, String email, String phoneNumber){
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public BankAccount(String customerName, String email, String phoneNumber) {
+        this("2872", 999, customerName, email, phoneNumber);
+    }
 
     public void setAccountNumber(String accountNumber){
         this.accountNumber = accountNumber;
@@ -54,18 +62,26 @@ public class BankAccount {
         return this.phoneNumber;
     }
 
-    public void depositFunds(int amount){
-        this.balance += amount;
-        System.out.println("Rs. " + amount + " deposited in " + this.customerName + "'s account. Available Balance: " + this.balance);
+    public void depositFunds(int depositAmount){
+        this.balance += depositAmount;
+        System.out.println("Rs. " + depositAmount + " deposited in " + this.customerName + "'s account. Available Balance: " + this.balance);
     }
 
-    public void withdrawalFunds(int amount){
-        if(this.balance < amount){
-            System.out.println("Balance Insufficient to withdrawal Rs. " + amount + ". Available Balance: " + this.balance);
+    public void withdrawalFunds(int withdrawalAmount){
+        if(this.balance < withdrawalAmount){
+            System.out.println("Balance Insufficient to withdrawal Rs. " + withdrawalAmount + ". Available Balance: " + this.balance);
             return;
         }
-        this.balance -= amount;
-        System.out.println("Rs. " + amount + " withdrawal from your account. Available Balance: " + this.balance);
+        this.balance -= withdrawalAmount;
+        System.out.println("Rs. " + withdrawalAmount + " withdrawal from your account. Available Balance: " + this.balance);
     }
 
+    public void accountDetails(){
+        System.out.println("Account Details:" +
+                "\nCustomer Name: " + this.customerName +
+                "\nAccount Number: " + this.accountNumber +
+                "\nBalance: " + this.balance +
+                "\nEmail: " + this.email +
+                "\nPhone Number: " + this.phoneNumber + "\n\n");
+    }
 }
