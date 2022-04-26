6dee3f83e5aa82c4b52eee01db84a2987fdc756a
abhishekchandra2522k <1ac23456789@gmail.com>
2022-04-26 21:52:58 +0530

    Update StaticMethods, add Static Variables

--- StaticMethods/src/Main.java
+++ StaticMethods/src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Calculator.printSum(5, 10);
-        printHello();
+        Calculator.printSum(5, 10); // prints "15"
+        printHello(); // prints "Hello"
+
+        Dog rex = new Dog("rex");
+        Dog fluffy = new Dog("fluffy");
+
+        rex.printName(); // prints "fluffy" as name and "rex" as realName
+        fluffy.printName(); // prints "fluffy" as name and "fluffy" as realName
+
     }
 
     public static void printHello(){
