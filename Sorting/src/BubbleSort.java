23f0ef2fd645d66c34fcb86d5c41a9c5f23e1150
abhishekchandra2522k <1ac23456789@gmail.com>
2022-10-29 22:39:29 +0530

    Add programs

---
+++ Sorting/src/BubbleSort.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {4, 1, 3, 2, 5};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void bubbleSort(int[] arr) {
+        boolean swapped;
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                    if(arr[j] > arr[j + 1]){
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swapped = true;
+                    }
+            }
+
+            if(!swapped){
+                break;
+            }
+        }
+
+    }
+
+    private static void swap(int i, int i1) {
+        int temp = i;
+        i = i1;
+        i1 = temp;
+    }
+
+
+}
