ac6abb5baa4c1f406459a46483c1c9b10b85c0bb
abhishekchandra2522k <1ac23456789@gmail.com>
2022-11-04 20:58:59 +0530

    Add program

---
+++ Recursion/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        message(5);
+    }
+
+    static void message(int i){
+        if(i == 1) {
+            return;
+        }
+        message(--i);
+        System.out.println("Hello World");
+    }
+}
