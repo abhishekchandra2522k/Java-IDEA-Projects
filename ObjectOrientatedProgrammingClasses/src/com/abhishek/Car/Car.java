986300f674821923793707b9bf9052af5f03d8bf
abhishekchandra2522k <1ac23456789@gmail.com>
2022-11-19 21:27:14 +0530

    Sync changes

--- ObjectOrientatedProgrammingClasses/src/com/abhishek/Car/Car.java
+++ ObjectOrientatedProgrammingClasses/src/com/abhishek/Car/Car.java
@@ -1,4 +1,4 @@
-package academy.learnprogramming;
+package com.abhishek.Car;
 
 public class Car {
     private int doors;
--- ObjectOrientatedProgrammingClasses/src/com/abhishek/Car/Main.java
+++ ObjectOrientatedProgrammingClasses/src/com/abhishek/Car/Main.java
@@ -1,4 +1,4 @@
-package academy.learnprogramming;
+package com.abhishek.Car;
 
 public class Main {
     public static void main(String[] args) {
