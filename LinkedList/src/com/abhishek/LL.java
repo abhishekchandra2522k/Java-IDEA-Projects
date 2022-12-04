ced5ce168626a787d8f92627187bae86fb98c06b
abhishekchandra2522k <1ac23456789@gmail.com>
2022-12-04 22:08:35 +0530

    Update LL, add duplicates remove code

--- LinkedList/src/com/abhishek/LL.java
+++ LinkedList/src/com/abhishek/LL.java
@@ -145,4 +145,35 @@ public class LL {
             this.next = next;
         }
     }
+
+    // questions
+    public void duplicates(){
+        Node node = head;
+        while(node.next != null) {
+            if (node.value == node.next.value){
+                node.next = node.next.next;
+                size--;
+            }
+            else{
+                node = node.next;
+            }
+        }
+
+        tail = node;
+        tail.next = null;
+    }
+
+
+    public static void main(String[] args) {
+        LL list = new LL(0);
+        list.insertLast(1);
+        list.insertLast(1);
+        list.insertLast(2);
+        list.insertLast(3);
+        list.insertLast(3);
+        list.insertLast(3);
+        list.display();
+        list.duplicates();
+        list.display();
+    }
 }
