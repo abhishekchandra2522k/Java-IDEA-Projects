b3c92db802f4bfac974bf4aa74afea9ae50312e4
abhishekchandra2522k <1ac23456789@gmail.com>
2022-12-18 22:37:41 +0530

    Add CircularQueue

--- StacksAndQueues/src/com/abhishek/QueueMain.java
+++ StacksAndQueues/src/com/abhishek/QueueMain.java
@@ -2,7 +2,22 @@ package com.abhishek;
 
 public class QueueMain {
     public static void main(String[] args) throws Exception {
-        CustomQueue queue = new CustomQueue(5);
+//        CustomQueue queue = new CustomQueue(5);
+
+//        queue.insert(3);
+//        queue.insert(6);
+//        queue.insert(5);
+//        queue.insert(19);
+//        queue.insert(1);
+//
+//        queue.display();
+//
+//        System.out.println(queue.remove());
+//
+//        queue.display();
+
+
+        CircularQueue queue = new CircularQueue(5);
 
         queue.insert(3);
         queue.insert(6);
@@ -13,7 +28,9 @@ public class QueueMain {
         queue.display();
 
         System.out.println(queue.remove());
-
+        queue.insert(133);
+        System.out.println(queue.remove());
+        queue.insert(30);
         queue.display();
     }
 }
